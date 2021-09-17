1bc32f897e6ae3fef87f3c1557da5779d4e99788
Chantal de Bakker <chantal1369@gmail.com>
2021-09-17 14:38:46 -0600

    add manual initialized MRI-CT registration

---
+++ ManualInitialGuessLibrary_py3.py
@@ -0,0 +1,351 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 10 14:27:32 2018
+
+@author: chantaldebakker
+"""
+
+import vtk
+import numpy as np
+
+def GetPoints(filePath, fixedfnm, movingfnm, fixed_type, moving_type):
+  while True:
+    print("Navigate 2D slices and pick 4 points in each.")
+
+    ReadMha(filePath,fixedfnm, movingfnm)
+    Navigate2D(fixed_type, moving_type)
+
+    print("When done, close out of window and type q")
+    optionUser = input("?: ")
+
+    if (optionUser == "q"):
+      fixed_pts = np.array([[0,0,0],[0,0,0],[0,0,0],[0,0,0]])
+      moving_pts = np.array([[0,0,0],[0,0,0],[0,0,0],[0,0,0]])
+      fixed_pts[0] = points_picked[0]
+      fixed_pts[0][1] = fixed.GetDimensions()[1] - fixed_pts[0][1]
+      moving_pts[0] = points_picked[1]
+      moving_pts[0][0] = moving_pts[0][0] - fixed.GetDimensions()[0]
+      moving_pts[0][1] = moving.GetDimensions()[1] - moving_pts[0][1]
+      fixed_pts[1] = points_picked[2]
+      fixed_pts[1][1] = fixed.GetDimensions()[1] - fixed_pts[1][1]
+      moving_pts[1] = points_picked[3]
+      moving_pts[1][0] = moving_pts[1][0] - fixed.GetDimensions()[0]
+      moving_pts[1][1] = moving.GetDimensions()[1] - moving_pts[1][1]
+      fixed_pts[2] = points_picked[4]
+      fixed_pts[2][1] = fixed.GetDimensions()[1] - fixed_pts[2][1]
+      moving_pts[2] = points_picked[5]
+      moving_pts[2][0] = moving_pts[2][0] - fixed.GetDimensions()[0]
+      moving_pts[2][1] = moving.GetDimensions()[1] - moving_pts[2][1]
+      fixed_pts[3] = points_picked[6]
+      fixed_pts[3][1] = fixed.GetDimensions()[1] - fixed_pts[3][1]
+      moving_pts[3] = points_picked[7]
+      moving_pts[3][0] = moving_pts[3][0] - fixed.GetDimensions()[0]
+      moving_pts[3][1] = moving.GetDimensions()[1] - moving_pts[3][1]
+
+      #Convert to absolute values by adding in offset and converting from voxels to absolute distance:
+      fixed_pts[0][0] = fixed_pts[0][0]*fixed.GetSpacing()[0]
+      fixed_pts[1][0] = fixed_pts[1][0]*fixed.GetSpacing()[0]
+      fixed_pts[2][0] = fixed_pts[2][0]*fixed.GetSpacing()[0]
+      fixed_pts[3][0] = fixed_pts[3][0]*fixed.GetSpacing()[0]
+      fixed_pts[0][1] = fixed_pts[0][1]*fixed.GetSpacing()[1]
+      fixed_pts[1][1] = fixed_pts[1][1]*fixed.GetSpacing()[1]
+      fixed_pts[2][1] = fixed_pts[2][1]*fixed.GetSpacing()[1]
+      fixed_pts[3][1] = fixed_pts[3][1]*fixed.GetSpacing()[1]
+      fixed_pts[0][2] = fixed_pts[0][2]*fixed.GetSpacing()[2]
+      fixed_pts[1][2] = fixed_pts[1][2]*fixed.GetSpacing()[2]
+      fixed_pts[2][2] = fixed_pts[2][2]*fixed.GetSpacing()[2]
+      fixed_pts[3][2] = fixed_pts[3][2]*fixed.GetSpacing()[2]
+
+      moving_pts[0][0] = moving_pts[0][0]*moving.GetSpacing()[0]
+      moving_pts[1][0] = moving_pts[1][0]*moving.GetSpacing()[0]
+      moving_pts[2][0] = moving_pts[2][0]*moving.GetSpacing()[0]
+      moving_pts[3][0] = moving_pts[3][0]*moving.GetSpacing()[0]
+      moving_pts[0][1] = moving_pts[0][1]*moving.GetSpacing()[1]
+      moving_pts[1][1] = moving_pts[1][1]*moving.GetSpacing()[1]
+      moving_pts[2][1] = moving_pts[2][1]*moving.GetSpacing()[1]
+      moving_pts[3][1] = moving_pts[3][1]*moving.GetSpacing()[1]
+      moving_pts[0][2] = moving_pts[0][2]*moving.GetSpacing()[2]
+      moving_pts[1][2] = moving_pts[1][2]*moving.GetSpacing()[2]
+      moving_pts[2][2] = moving_pts[2][2]*moving.GetSpacing()[2]
+      moving_pts[3][2] = moving_pts[3][2]*moving.GetSpacing()[2]
+
+      return fixed_pts.tolist(), moving_pts.tolist()
+      break
+
+
+
+
+
+def ReadMha(filePath, fixed_fnm, moving_fnm):
+  print("---------------------------Reading image---------------------------")
+
+  # Define a mha reader from vtk.
+  MhaReader = vtk.vtkMetaImageReader()
+
+  #Read the path that the user will input.
+  fnm1 = filePath+'/'+fixed_fnm+'.mha'
+  MhaReader.SetFileName(fnm1)
+  MhaReader.Update()
+
+  #Store the image in a global variable, so that it can be accesed later in any part of the code.
+  global fixed
+  fixed = MhaReader.GetOutput()
+  print(fixed.GetDimensions())
+
+
+  # Define a mha reader from vtk.
+  MhaReader2 = vtk.vtkMetaImageReader()
+
+ #Read the path that the user will input.
+  fnm2 = filePath+'/'+moving_fnm+'.mha'
+  MhaReader2.SetFileName(fnm2)
+  MhaReader2.Update()
+
+  #Store the image in a global variable, so that it can be accesed later in any part of the code.
+  global moving
+  moving = MhaReader2.GetOutput()
+  print(moving.GetDimensions())
+
+# This function is used to define the image orientation as axial, coronal, or sagittal.
+# Note how it receives two parameters: imageToOrient, and orientation
+def orientImage (imageToOrient, orientation):
+  (xMin, xMax, yMin, yMax, zMin, zMax) = imageToOrient.GetExtent()
+  (xSpacing, ySpacing, zSpacing) = imageToOrient.GetSpacing()
+  (x0, y0, z0) = imageToOrient.GetOrigin()
+
+  # Calculate the coordinates of the center of the image.
+  center = [x0 + xSpacing * (xMin + xMax),
+            y0 + ySpacing * (yMin + yMax),
+            z0 + zSpacing * (zMin + zMax)]
+
+  #This matrices are used to transform the coordinates of the image, for each orientation.
+  axial = vtk.vtkMatrix4x4()
+  axial.DeepCopy((1, 0, 0,center[0] ,
+                  0, -1, 0,center[1] ,
+                  0, 0, 1,center[2],
+                  0, 0, 0, 1))
+
+  coronal = vtk.vtkMatrix4x4()
+  coronal.DeepCopy((1, 0, 0, center[0],
+                    0, 0, 1, center[1],
+                    0, -1, 0, center[2],
+                    0, 0, 0, 1))
+
+  sagittal = vtk.vtkMatrix4x4()
+  sagittal.DeepCopy((0, 0,-1, center[0],
+                     1, 0, 0, center[1],
+                     0, -1, 0,center[2] ,
+                    0, 0, 0, 1))
+
+  flipimage = vtk.vtkMatrix4x4()
+  flipimage.DeepCopy((-1, 0, 0, center[0],
+                      0, -1, 0, center[1],
+                      0, 0, -1, center[2],
+                      0, 0, 0, 1))
+
+  # vtkImageReslice orients an image, given a transformation matrix.
+  reslice = vtk.vtkImageReslice()
+  reslice.SetInputData(imageToOrient)
+  reslice.SetOutputDimensionality(3)
+  reslice.SetResliceAxesOrigin(0,0,0)
+
+  if orientation=='axial':
+    reslice.SetResliceAxes(axial)
+  elif orientation=='coronal':
+    reslice.SetResliceAxes(coronal)
+  elif orientation=='sagittal':
+    reslice.SetResliceAxes(sagittal)
+  elif orientation=='flipimage':
+    reslice.SetResliceAxes(flipimage)
+
+  # interpolation is required to re-slice the image in the required orientation
+  reslice.SetInterpolationModeToCubic()
+  reslice.Update()
+
+  return reslice.GetOutput()
+
+# This is the main navigation function
+def Navigate2D(fixed_type, moving_type):
+  print("---------------------------Navigating 2D---------------------------")
+
+  global orientation_fixed
+  orientation_fixed = 'axial'
+  global orientation_moving
+  orientation_moving = 'axial'
+
+#  if (fixed_type == 'CT'):
+#      orientation_fixed = 'axial'
+#  elif (fixed_type == 'MR'):
+#      orientation_fixed = 'coronal'
+#  global orientation_moving
+#  if (moving_type == 'CT'):
+#      orientation_moving = 'axial'
+#  elif (moving_type == 'MR'):
+#      orientation_moving = 'coronal'
+  global zSlice_fixed
+  global zSlice_moving
+  global clickCount
+  clickCount = 0
+  zSlice_fixed = 150
+  zSlice_moving = 150
+
+#  if (optionUser == "1"):
+#    orientation = "axial"
+#  if (optionUser == "2"):
+#    orientation = "coronal"
+#  if (optionUser == "3"):
+#    orientation = "sagittal"
+
+#  orientation = "axial"
+
+  #display fixed image:
+  fixed_imageNavigate2D = orientImage(fixed, orientation_fixed)
+
+  global mapperNavigate2D
+  mapperNavigate2D = vtk.vtkImageMapper()
+  mapperNavigate2D.SetInputData(fixed_imageNavigate2D)
+  mapperNavigate2D.SetZSlice(zSlice_fixed)
+  # window and level have to be adjusted for better visualization
+  if (fixed_type == 'MR'):
+      # mapperNavigate2D.SetColorWindow(623)
+      mapperNavigate2D.SetColorWindow(200)
+      mapperNavigate2D.SetColorLevel(100)
+
+  elif (fixed_type == 'CT'):
+      # mapperNavigate2D.SetColorWindow(12900)
+      mapperNavigate2D.SetColorWindow(6000)
+      mapperNavigate2D.SetColorLevel(0)
+
+  actorNavigate2D = vtk.vtkActor2D()
+  actorNavigate2D.SetMapper(mapperNavigate2D)
+
+  global renderWindowNavigate2D
+  rendererNavigate2D = vtk.vtkRenderer()
+  rendererNavigate2D.AddActor(actorNavigate2D)
+
+  renderWindowNavigate2D = vtk.vtkRenderWindow()
+  renderWindowNavigate2D.AddRenderer(rendererNavigate2D)
+  rendererNavigate2D.SetViewport(0,0,0.5,1)
+
+
+#  del renderWindowNavigate2D, windowInteractorN2D
+
+  #display moving image:
+  moving_imageNavigate2D = orientImage(moving, orientation_moving)
+#  if (fixed_type == 'MR'):
+  #    if (moving_type == 'CT'):
+  #        moving_imageNavigate2D = orientImage(moving, 'flipimage')
+#      else:
+#          moving_imageNavigate2D = orientImage(moving, orientation_moving)
+#  else:
+#      moving_imageNavigate2D = orientImage(moving, orientation_moving)
+
+  global moving_mapperNavigate2D
+  moving_mapperNavigate2D = vtk.vtkImageMapper()
+  moving_mapperNavigate2D.SetInputData(moving_imageNavigate2D)
+  moving_mapperNavigate2D.SetZSlice(zSlice_moving)
+  # window and level have to be adjusted for better visualization
+  # moving_mapperNavigate2D.SetColorWindow(200)
+  # moving_mapperNavigate2D.SetColorLevel(100)
+  moving_mapperNavigate2D.SetColorWindow(600)
+  moving_mapperNavigate2D.SetColorLevel(300)
+  # moving_mapperNavigate2D.SetColorWindow(200)
+  # moving_mapperNavigate2D.SetColorLevel(100)
+
+  moving_actorNavigate2D = vtk.vtkActor2D()
+  moving_actorNavigate2D.SetMapper(moving_mapperNavigate2D)
+
+  moving_rendererNavigate2D = vtk.vtkRenderer()
+  moving_rendererNavigate2D.AddActor(moving_actorNavigate2D)
+
+  renderWindowNavigate2D.AddRenderer(moving_rendererNavigate2D)
+  moving_rendererNavigate2D.SetViewport(0.5,0,1,1)
+
+  renderWindowNavigate2D.SetSize(fixed.GetDimensions()[0]+moving.GetDimensions()[0], max(fixed.GetDimensions()[1], moving.GetDimensions()[1]))
+
+
+  windowInteractorN2D = vtk.vtkRenderWindowInteractor()
+  windowInteractorN2D.SetRenderWindow(renderWindowNavigate2D)
+
+  #Note here how the event is added to the observer. The name of the event is fixed, the name
+  #of the function that handles the event is user defined.
+  windowInteractorN2D.AddObserver("KeyPressEvent", KeyPressNavigate2D, 1.0)
+  # windowInteractorN2D.AddObserver("CharEvent", KeyPressNavigate2D, 1.0)
+
+
+  #initialize points list:
+  global points_picked
+  points_picked = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+
+  #look for mouse clicks to select points to initialize registration
+  windowInteractorN2D.AddObserver("RightButtonPressEvent", PickPoints, 1.0)
+
+
+  windowInteractorN2D.Initialize()
+  renderWindowNavigate2D.Render()
+  windowInteractorN2D.Start()
+
+  del renderWindowNavigate2D, windowInteractorN2D
+
+def KeyPressNavigate2D(obj, Event):
+  key = obj.GetKeySym()
+  limitUp_fixed = 0
+  limitDown_fixed = 0
+  limitUp_moving = 0
+  limitDown_moving = 0
+
+  limitUp_fixed = fixed.GetDimensions()[2] - 1
+  limitUp_moving = moving.GetDimensions()[2] - 1
+
+#  if (orientation_fixed == "axial"):
+#    limitUp_fixed = fixed.GetDimensions()[2] - 1
+#  if (orientation_moving == "axial"):
+#    limitUp_moving = moving.GetDimensions()[2] - 1
+#  if (orientation_fixed == "coronal"):
+#    limitUp_fixed = fixed.GetDimensions()[1] - 1
+#  if (orientation_moving == "coronal"):
+#    limitUp_moving = moving.GetDimensions()[1] - 1
+#  if (orientation_fixed == "sagittal"):
+#    limitUp_fixed = fixed.GetDimensions()[0] - 1
+#  if (orientation_moving == "sagittal"):
+#    limitUp_moving = moving.GetDimensions()[0] - 1
+
+  global zSlice_fixed
+  global zSlice_moving
+
+  if (key == "Up"):
+    zSlice_fixed = zSlice_fixed + 1
+    if (zSlice_fixed > limitUp_fixed):
+      zSlice_fixed = limitUp_fixed
+
+  if (key == "Down"):
+    zSlice_fixed = zSlice_fixed - 1
+    if (zSlice_fixed < 0):
+      zSlice_fixed = 0
+
+  if (key == "Right"):
+    zSlice_moving = zSlice_moving + 1
+    if (zSlice_moving > limitUp_moving):
+      zSlice_moving = limitUp_moving
+
+  if (key == "Left"):
+    zSlice_moving = zSlice_moving - 1
+    if (zSlice_moving < 0):
+      zSlice_moving = 0
+
+  mapperNavigate2D.SetZSlice(zSlice_fixed)
+  moving_mapperNavigate2D.SetZSlice(zSlice_moving)
+  renderWindowNavigate2D.Render()
+
+def PickPoints(obj, Event):
+  global clickCount
+  [xpos,ypos] = obj.GetEventPosition()
+  if clickCount == 0 or clickCount == 2 or clickCount == 4 or clickCount == 6:
+      zpos = zSlice_fixed
+  elif clickCount == 1 or clickCount == 3 or clickCount == 5 or clickCount == 7:
+      zpos = zSlice_moving
+  print("mouse clicked: ", (xpos, ypos, zpos), clickCount)
+  global points_picked
+  points_picked[clickCount] = [xpos, ypos, zpos]
+  clickCount = clickCount + 1
---
+++ Register_PDMRI-DECT_LandmarkInitialized.py
@@ -0,0 +1,259 @@
+from __future__ import print_function
+from functools import reduce
+
+
+import SimpleITK as sitk
+import sys
+import os
+import ManualInitialGuessLibrary_py3
+import numpy as np
+import pandas as pd
+import argparse
+from bonelab.util.echo_arguments import echo_arguments
+
+
+def command_iteration(method) :
+    print("{0:3} = {1:7.5f} : {2}".format(method.GetOptimizerIteration(),
+                                           method.GetMetricValue(),
+                                           method.GetOptimizerPosition()))
+
+
+
+def FindLabels(patient_id):
+    label_mat = np.loadtxt(open("SegLabels.csv", "rb"), delimiter=",", skiprows=1, usecols=(1,2,3,4))
+    patient_num = int(patient_id[len(patient_id)-4:])
+
+    f_i = label_mat[patient_num-1,0]
+    t_i = label_mat[patient_num-1,1]
+    f_c = label_mat[patient_num-1,2]
+    t_c = label_mat[patient_num-1,3]
+
+    return f_i,t_i,f_c,t_c
+
+def FindInjuredSide(patient_id):
+    side_mat = np.loadtxt(open("SegLabels.csv", "rb"), delimiter=",", skiprows=1, usecols=(4,5))
+    patient_num = int(patient_id[len(patient_id)-4:])
+
+    side_num = side_mat[patient_num-1,1]
+    return side_num
+
+def register_PD_CT(filePath,participant_id,CT_fnm,MRI_fnm,mask_fnm,bone_id):
+
+    pixelType = sitk.sitkFloat32
+
+    patient_id = 'SALTACII_0027'
+    bone = 'Tibia'
+
+
+    f_i,t_i,f_c,t_c = FindLabels(participant_id)
+    if bone == 'Femur':
+        BONE_LABEL = f_i
+    elif bone == 'Tibia':
+        BONE_LABEL = t_i
+
+
+    side_num = FindInjuredSide(participant_id)
+    if side_num == 0:
+        injured_side = 'left'
+    elif side_num == 1:
+        injured_side = 'right'
+
+    fixedfnm = CT_fnm
+    fixed = sitk.ReadImage(filePath+'/'+fixedfnm+'.mha', sitk.sitkFloat32)
+    fixed_type = 'CT'
+    img_size = fixed.GetSize()
+
+    movingfnm = MRI_fnm
+    moving = sitk.ReadImage(filePath+'/'+movingfnm+'.mha', sitk.sitkFloat32)
+    moving_type = 'MR'
+
+    #Crop CT images to focus on injured side only:
+    crop_DE = sitk.CropImageFilter()
+
+
+    if injured_side == 'left':
+        crop_DE.SetLowerBoundaryCropSize([np.int(img_size[0]/2),0,0])
+        crop_DE.SetUpperBoundaryCropSize([0,0,0])
+    elif injured_side == 'right':
+        crop_DE.SetLowerBoundaryCropSize([0,0,0])
+        crop_DE.SetUpperBoundaryCropSize([np.int(img_size[0]/2),0,0])
+
+
+    fixed_cropped = crop_DE.Execute(fixed)
+    fixed_cropped.SetOrigin((0,0,0))
+    sitk.WriteImage(fixed_cropped, filePath+'/'+fixedfnm+'_cropped.mha',True)
+
+    seg_img = sitk.ReadImage(filePath+'/'+mask_fnm+'.mha')
+    seg_img = crop_DE.Execute(seg_img)
+    seg_img.SetOrigin((0,0,0))
+    sitk.WriteImage(seg_img, filePath+'/'+fixedfnm+'_SEG_cropped.mha',True)
+
+    fixed_image_mask = seg_img==BONE_LABEL
+    sitk.WriteImage(fixed_image_mask, filePath+'/'+fixedfnm+'_SEG_cropped_'+bone+'.mha',True)
+
+    #create a mask for image registration that is focused on the cortical region only (with some dilation in both directions)
+    #excludes most of marrow and external soft tissues
+    radius = 5
+    dilated_fixed_image_mask = sitk.BinaryDilate(
+            fixed_image_mask,
+            radius
+            )
+    eroded_fixed_image_mask = sitk.BinaryErode(
+            fixed_image_mask,
+            radius
+            )
+
+    fixed_image_mask = dilated_fixed_image_mask - eroded_fixed_image_mask
+
+
+    sitk.WriteImage(fixed_image_mask, filePath+'/'+fixedfnm+'_Dilated_mask.mha',True)
+
+
+    moving_cropped = moving
+    moving_cropped.SetOrigin((0,0,0))
+
+
+    #This initial transformation is just to get the MRI image in the same dimensions as DE-CT
+    initial_transformation1 = sitk.CenteredTransformInitializer(fixed_cropped,
+                                                      moving_cropped,
+                                                      sitk.Euler3DTransform(),
+                                                      sitk.CenteredTransformInitializerFilter.GEOMETRY)
+
+    print(initial_transformation1)
+    resample1 = sitk.ResampleImageFilter()
+    resample1.SetReferenceImage(fixed_cropped)
+
+    resample1.SetInterpolator(sitk.sitkLinear)
+    resample1.SetTransform(initial_transformation1)
+    sitk.WriteImage(resample1.Execute(moving_cropped), filePath+'/'+movingfnm+'-centered_'+bone+'.mha',True)
+
+
+    # Manually select landmarks, and generate transformation based on the landmarks
+    fixed_initialpts, moving_initialpts = ManualInitialGuessLibrary_py3.GetPoints(filePath,fixedfnm+'_cropped',movingfnm+'-centered_'+bone,fixed_type,moving_type)
+
+    d2 = {'Landmark1_fixed': fixed_initialpts[0],'Landmark2_fixed': fixed_initialpts[1], 'Landmark3_fixed': fixed_initialpts[2], 'Landmark4_fixed': fixed_initialpts[3], 'Landmark1_moving': moving_initialpts[0], 'Landmark2_moving': moving_initialpts[1], 'Landmark3_moving': moving_initialpts[2], 'Landmark4_moving': moving_initialpts[3]}
+    df2 = pd.DataFrame(data=d2)
+    df2.to_csv(fixedfnm+'_Landmarks_'+bone+'.csv')
+
+    print('fixed_initialpts', fixed_initialpts)
+    print('moving_initialpts', moving_initialpts)
+
+
+
+    fixed_image_points_flat = [c for p in fixed_initialpts for c in p]
+    moving_image_points_flat = [c for p in moving_initialpts for c in p]
+
+
+    initial_transformation2 = sitk.LandmarkBasedTransformInitializer(sitk.VersorRigid3DTransform(),
+                                                                fixed_image_points_flat,
+                                                                moving_image_points_flat)
+
+    print(initial_transformation2)
+
+    #Combine the 2 transformations
+    initial_transformation = sitk.Transform(initial_transformation1)
+    initial_transformation.AddTransform(initial_transformation2)
+
+    resample2 = sitk.ResampleImageFilter()
+    resample2.SetReferenceImage(fixed_cropped)
+
+    resample2.SetInterpolator(sitk.sitkLinear)
+    resample2.SetTransform(initial_transformation2)
+    sitk.WriteImage(resample2.Execute(resample1.Execute(moving_cropped)), filePath+'/'+movingfnm+'-manualinitialguess_'+bone+'.mha',True)
+
+
+
+    sitk.WriteTransform(initial_transformation, filePath+'/'+participant_id+'_T1-CT_manualinitialguess_'+bone+'.tfm')
+
+    #landmark-initialized registration:
+    R = sitk.ImageRegistrationMethod()
+    R.SetMetricAsMattesMutualInformation(numberOfHistogramBins=256)
+    R.SetMetricSamplingStrategy(R.REGULAR)
+    R.SetOptimizerAsPowell()
+    R.SetOptimizerScalesFromPhysicalShift()
+    R.SetInitialTransform(initial_transformation)
+    R.SetInterpolator(sitk.sitkLinear)
+
+    R.SetMetricFixedMask(fixed_image_mask)
+
+    def callback(R):
+        i = R.GetOptimizerIteration()
+        m = R.GetMetricValue()
+        print(i,m)
+
+    R.AddCommand(sitk.sitkIterationEvent, lambda: callback(R))
+
+
+    outTx = R.Execute(sitk.Normalize(fixed_cropped), sitk.Normalize(moving_cropped))
+    sitk.WriteTransform(outTx, filePath+'/'+participant_id+'_T1-CT_registration_'+bone+'.tfm')
+
+
+    print("-------")
+    print(outTx)
+    print("Optimizer stop condition: {0}".format(R.GetOptimizerStopConditionDescription()))
+    print(" Iteration: {0}".format(R.GetOptimizerIteration()))
+    print(" Metric value: {0}".format(R.GetMetricValue()))
+
+
+    resample = sitk.ResampleImageFilter()
+    resample.SetReferenceImage(fixed_cropped)
+
+    resample.SetInterpolator(sitk.sitkLinear)
+    resample.SetTransform(outTx)
+    sitk.WriteImage(resample.Execute(moving_cropped), filePath+'/'+movingfnm+'-registered_'+bone+'.mha',True)
+
+def main():
+    # Set up description
+    description='''Function to register left and right knees within one individual.
+
+    This program will read in a bilateral knee CT (acquired using SALTACII protocol), and crop the image to separate the contralateral and injured knees (identified in commandline arguments)
+    The contralateral knee will then be mirrored to match the injured knee, and image registration will be performed to align the mirrored contralateral and injured sides.
+    Separate registrations should be performed for the femur & tibia.
+
+
+    Input image = simulated monoenergetic image (e.g., at 40 keV), mask of major bones (generated through method of Krčah, Marcel, Gábor Székely, and Rémi Blanc, IEEE, 2011.)
+
+
+'''
+
+
+    # Set up argument parsing
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        prog="Register_PDMRI-DECT_LandmarkInitialized",
+        description=description
+    )
+
+    parser.add_argument("filePath",
+                        type=str,
+                        help="The filepath")
+    parser.add_argument("participant_id",
+                        type=str,
+                        help="The participant ID")
+    parser.add_argument("--CT_fnm","-ctf",
+                        default='40keV',
+                        type=str,
+                        help="Filename for the simululated monoenergetic CT image")
+    parser.add_argument("--MRI_fnm","-mrf",
+                        default='Sag_PD_flipYZ_flipXY',
+                        type=str,
+                        help="Filename for the PD or T1 weighted MR image (flipped into axial coordinates)")
+    parser.add_argument("--mask_fnm","-m",
+                        default='Calibrated_StandardSEQCT_SEG',
+                        type=str,
+                        help="Filename for the periosteal mask image of major bones")
+    parser.add_argument("--bone_id","-b",
+                        default='Tibia',
+                        type=str,
+                        help="Bone of interest (Femur or Tibia)")
+
+    # Parse and display
+    args = parser.parse_args()
+    print(echo_arguments('DECT_LandmarkInitialized', vars(args)))
+
+    # Run program
+    register_PD_CT(**vars(args))
+
+
+if __name__ == '__main__':
+    main()
